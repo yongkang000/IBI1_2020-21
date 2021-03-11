4193c246078a2b188cf9758e50b773e5fc76386d
yongkang000 <79959798+yongkang000@users.noreply.github.com>
2021-03-11 16:33:36 +0800

    Update fibo.py

--- Practical5/fibo.py
+++ Practical5/fibo.py
@@ -1,8 +1,8 @@
-def fibo(n):
+def fibo(n): #define a function to make it clearer
   if n<=1:
     return n
   else:
-    return fibo(n-1) + fibo(n-2)
+    return fibo(n-1) + fibo(n-2) # make it back from n to 1
 n=13
-for i in range(1，n+1):
+for i in range(1，n+1): #print from 1 to n result
   print(fibo(i))
