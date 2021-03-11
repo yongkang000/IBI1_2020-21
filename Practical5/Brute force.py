f5508a072488b2cc48791e3d9215ce9d9d91f8b2
yongkang000 <79959798+yongkang000@users.noreply.github.com>
2021-03-11 16:42:06 +0800

    Update Brute force.py

--- Practical5/Brute force.py
+++ Practical5/Brute force.py
@@ -1,9 +1,9 @@
-mima =input('请设置密码（6位数）')
+password =input('plesae set your password with 6 number')
 for i in range(0,1000000):
-  if i == int(mima):
+  if i == int(password):
     break
 if i==0:
-  print('破解结果：你的密码是：'+'000000')
+  print('The result：your password is'+'000000')
 else:
   n=0
   while i<1000000:
@@ -12,4 +12,4 @@ else:
   m=0
   for x in range(n):
     i= i/10
-  print( '破解结果：你的密码是：' +'0'*(n-1)+ str(int(i)) )
+  print( 'The result：your password is' +'0'*(n-1)+ str(int(i)) )
